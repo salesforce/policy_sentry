3460c8a25780fcc429a5b6d20edd9f4e759464f9
Kinnaird McQuade <kmcquade@salesforce.com>
2019-12-11 00:42:50 -0500

    Fix pylint

--- policy_sentry/shared/scrape.py
+++ policy_sentry/shared/scrape.py
@@ -1,12 +1,9 @@
 """
 At policy_sentry initialize time, this function is used to scrape the AWS HTML docs to build the sqlite database.
 """
-import os
 import yaml
 import pandas
 from policy_sentry.shared.constants import LINKS_YML_FILE_LOCAL
-# from policy_sentry.shared.awsdocs import get_list_of_service_prefixes_from_links_file
-# ALL_AWS_SERVICES = get_list_of_service_prefixes_from_links_file()
 
 
 def get_html(directory, requested_service):
@@ -31,7 +28,6 @@ def get_html(directory, requested_service):
         except ValueError as v_e:
             if 'No tables found' in str(v_e):
                 print(f"No tables found for {link}")
-                pass
             else:
                 raise v_e
 
