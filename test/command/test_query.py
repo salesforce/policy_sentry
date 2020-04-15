bb8587dbe29accb5e0c89e588212681a8cc7c111
Kinnaird McQuade <kmcquade@salesforce.com>
2020-04-15 13:09:37 -0400

    Added logging for query functions

--- test/command/test_query.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import json
-import os
-from policy_sentry.command.query import query_condition_table, query_action_table, query_arn_table
-
-# class CommandQueryTest(unittest.TestCase):
-#     def test_
