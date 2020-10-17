86b435750f8d8ff43e06da7f90d15f0aa75af84d
Kinnaird McQuade <kmcquade@salesforce.com>
2020-10-17 13:25:47 -0400

    Added more test cases for the minimizing functions

--- test/writing/test_minimize.py
+++ test/writing/test_minimize.py
@@ -58,7 +58,6 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
         }
         sid_grp = SidGroup()
         write_format = sid_grp.process_template(cfg, minimize=0)
-        print(json.dumps(write_format, indent=4))
 
         """
         Expected result:
@@ -150,7 +149,6 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
         }
         sid_grp = SidGroup()
         write_format = sid_grp.process_template(cfg, minimize=1)
-        print(json.dumps(write_format, indent=4))
         """
         Expected result:
 
@@ -190,3 +188,92 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
         self.assertEqual(write_format['Statement'][1]['Action'], ['ssm:deletepar*', 'ssm:la*', 'ssm:putp*'],
                          "extra actions are returned")
         self.assertEqual(write_format['Statement'][1]['Resource'], cfg['write'], "Wrong resources were returned")
+
+    def test_minimize_arn_case_bucket(self):
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:s3:::bucket_name"],
+            "write": ["arn:aws:s3:::bucket_name"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+
+        # print(json.dumps(results, indent=4))
+        # To future-proof this unit test...
+        # (1) check that there is only one SID so it was combined during minimization
+        sid_names = get_sid_names_from_policy(results)
+        # For S3, it does require iam:PassRole lol because of s3:PutReplicationConfiguration requiring it as an action
+        self.assertEqual(len(sid_names), 2, "More than 2 statements returned, expected 2")
+
+    def test_minimize_arn_case_1(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:codecommit:us-east-1:123456789012:MyDemoRepo"],
+            "write": ["arn:aws:codecommit:us-east-1:123456789012:MyDemoRepo"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+    def test_minimize_arn_case_3(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:kinesis:us-east-1:account-id:firehose/myfirehose/consumer/someconsumer:${ConsumerCreationTimpstamp}"],
+            "write": ["arn:aws:kinesis:us-east-1:account-id:firehose/myfirehose/consumer/someconsumer:${ConsumerCreationTimpstamp}"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+    def test_minimize_arn_case_4(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:batch:region:account-id:job-definition/job-name:revision"],
+            "write": ["arn:aws:batch:region:account-id:job-definition/job-name:revision"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+    def test_minimize_arn_case_5(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:states:region:account-id:stateMachine:stateMachineName"],
+            "write": ["arn:aws:states:region:account-id:stateMachine:stateMachineName"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+    def test_minimize_arn_case_6(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:states:region:account-id:execution:stateMachineName:executionName"],
+            "write": ["arn:aws:states:region:account-id:execution:stateMachineName:executionName"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+    def test_minimize_arn_case_7(self):
+        """minimization test with ARN types from test_does_arn_match_case_1"""
+        cfg = {
+            "mode": "crud",
+            "read": ["arn:aws:greengrass:${Region}:${Account}:/greengrass/definition/devices/1234567/versions/1"],
+            "write": ["arn:aws:greengrass:${Region}:${Account}:/greengrass/definition/devices/1234567/versions/1"]
+        }
+        sid_group = SidGroup()
+        results = sid_group.process_template(cfg, minimize=0)
+        sid_names = get_sid_names_from_policy(results)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
