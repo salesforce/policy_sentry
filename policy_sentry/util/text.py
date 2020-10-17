be201cb0107eae91dfdd93ff7c28301963e82430
Kinnaird McQuade <kmcquade@salesforce.com>
2020-10-17 13:09:07 -0400

    Ensured minimization won't break if resource path contains special characters; improve the unit tests for minimization

--- policy_sentry/util/text.py
+++ policy_sentry/util/text.py
@@ -10,3 +10,11 @@ def capitalize_first_character(some_string):
     :return:
     """
     return " ".join("".join([w[0].upper(), w[1:].lower()]) for w in some_string.split())
+
+
+def strip_special_characters(some_string):
+    """Remove all special characters, punctuation, and spaces from a string"""
+    # Input: "Special $#! characters   spaces 888323"
+    # Output: 'Specialcharactersspaces888323'
+    result = ''.join(e for e in some_string if e.isalnum())
+    return result
--- test/writing/test_minimize.py
+++ test/writing/test_minimize.py
@@ -1,8 +1,9 @@
 import unittest
-
+import json
 from policy_sentry.writing.sid_group import SidGroup
 from policy_sentry.writing.minimize import minimize_statement_actions
 from policy_sentry.querying.all import get_all_actions
+from policy_sentry.util.policy_files import get_sid_names_from_policy, get_statement_from_policy_using_sid
 
 
 class MinimizeWildcardActionsTestCase(unittest.TestCase):
@@ -44,6 +45,47 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
             sorted(desired_result),
         )
 
+    def test_minimize_rw_same_one(self):
+        cfg = {
+            "mode": "crud",
+            "name": "",
+            "read": [
+                "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter",
+            ],
+            "write": [
+                "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter",
+            ]
+        }
+        sid_grp = SidGroup()
+        write_format = sid_grp.process_template(cfg, minimize=0)
+        print(json.dumps(write_format, indent=4))
+
+        """
+        Expected result:
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "SsmMultParametermyparameter",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:getpar*",
+                        "ssm:deletepar*",
+                        "ssm:la*",
+                        "ssm:putp*"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter"
+                    ]
+                }
+            ]
+        }
+        """
+        # To future-proof this unit test...
+        # (1) check that there is only one SID so it was combined during minimization
+        sid_names = get_sid_names_from_policy(write_format)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
     def test_minimize_rw_same(self):
         cfg = {
             "mode": "crud",
@@ -58,10 +100,38 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
             ]
         }
         sid_grp = SidGroup()
-        write_format = sid_grp.process_template(cfg,minimize=1)
-        write_format.pop('Version')
-        self.assertEqual(len(write_format['Statement']), 1, "More than one statement returned, expected 1")
-        expected_action = ['ssm:getpar*','ssm:deletepar*','ssm:la*','ssm:putp*']
+        write_format = sid_grp.process_template(cfg, minimize=1)
+        # print(json.dumps(write_format, indent=4))
+        """
+        the output will look like:
+
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "SsmParameterMyparameter",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:getpar*",
+                        "ssm:deletepar*",
+                        "ssm:la*",
+                        "ssm:putp*"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter",
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter2"
+                    ]
+                }
+            ]
+        }
+        """
+        # To future-proof this unit test...
+        # (1) check that there is only one SID so it was combined during minimization
+        sid_names = get_sid_names_from_policy(write_format)
+        self.assertEqual(len(sid_names), 1, "More than one statement returned, expected 1")
+
+        # (2) Check for the presence of certain actions that we know will be there
+        expected_action = ['ssm:getpar*', 'ssm:deletepar*', 'ssm:la*', 'ssm:putp*']
         self.assertEqual(write_format['Statement'][0]['Action'], expected_action, "extra actions are returned")
         self.assertEqual(write_format['Statement'][0]['Resource'], cfg['read'], "Wrong resources were returned")
 
@@ -79,12 +149,44 @@ class MinimizeWildcardActionsTestCase(unittest.TestCase):
             ]
         }
         sid_grp = SidGroup()
-        write_format = sid_grp.process_template(cfg,minimize=1)
-        write_format.pop('Version')
-        self.assertEqual(len(write_format['Statement']), 2, "More than one statement returned, expected 1")
+        write_format = sid_grp.process_template(cfg, minimize=1)
+        print(json.dumps(write_format, indent=4))
+        """
+        Expected result:
+
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "SsmReadParameter",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:getpar*"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter",
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter2"
+                    ]
+                },
+                {
+                    "Sid": "SsmWriteParameter",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:deletepar*",
+                        "ssm:la*",
+                        "ssm:putp*"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter",
+                        "arn:aws:ssm:us-east-1:123456789012:parameter/myparameter10"
+                    ]
+                }
+            ]
+        }
+        """
+        self.assertEqual(len(write_format['Statement']), 2, "More than two statements returned, expected 2")
         self.assertEqual(write_format['Statement'][0]['Action'], ['ssm:getpar*'], "extra actions are returned")
         self.assertEqual(write_format['Statement'][0]['Resource'], cfg['read'], "Wrong resources were returned")
         self.assertEqual(write_format['Statement'][1]['Action'], ['ssm:deletepar*', 'ssm:la*', 'ssm:putp*'],
                          "extra actions are returned")
         self.assertEqual(write_format['Statement'][1]['Resource'], cfg['write'], "Wrong resources were returned")
-
