62b73e1749f24569793d0a5aad2de47b026d99cd
Kinnaird McQuade <kmcquade@salesforce.com>
2020-11-10 09:24:57 -0500

    Clean up variables file and readme in Terraform modules

--- terraform_module/ps-template/variables.tf
+++ terraform_module/ps-template/variables.tf
@@ -11,66 +11,66 @@ variable "minimize" {
 
 variable "read_access_level" {
   description = "Provide a list of Amazon Resource Names (ARNs) that your role needs READ access to."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "write_access_level" {
   description = "Provide a list of Amazon Resource Names (ARNs) that your role needs WRITE access to."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "list_access_level" {
   description = "Provide a list of Amazon Resource Names (ARNs) that your role needs LIST access to."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "tagging_access_level" {
   description = "Provide a list of Amazon Resource Names (ARNs) that your role needs TAGGING access to."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "permissions_management_access_level" {
   description = "Provide a list of Amazon Resource Names (ARNs) that your role needs PERMISSIONS MANAGEMENT access to."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_single_actions" {
   description = "Individual actions that do not support resource constraints. For example, s3:ListAllMyBuckets"
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_read_service" {
   description = "To generate a list of AWS service actions that (1) are at the READ access level and (2) do not support resource constraints, list the service prefix here."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_write_service" {
   description = "To generate a list of AWS service actions that (1) are at the WRITE access level and (2) do not support resource constraints, list the service prefix here."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_list_service" {
   description = "To generate a list of AWS service actions that (1) are at the LIST access level and (2) do not support resource constraints, list the service prefix here."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_tagging_service" {
   description = "To generate a list of AWS service actions that (1) are at the TAGGING access level and (2) do not support resource constraints, list the service prefix here."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
 
 variable "wildcard_only_permissions_management_service" {
   description = "To generate a list of AWS service actions that (1) are at the PERMISSIONS MANAGEMENT access level and (2) do not support resource constraints, list the service prefix here."
-  type        = list
-  default     = [""]
+  type        = list(string)
+  default     = []
 }
