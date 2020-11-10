390e9c4ff8c3dd109034fcc1cce6ddf1039dfc24
Kinnaird McQuade <kmcquade@users.noreply.github.com>
2020-11-10 09:19:43 -0500

    Migrate terraform module (#277)
    Migrate Terraform module to salesforce org; Clean up .gitignore

---
+++ terraform_module/iam-policies/outputs.tf
@@ -0,0 +1,24 @@
+output "iam_policy_id" {
+  description = "The policy's ID."
+  value       = aws_iam_policy.policy.*.id
+}
+
+output "iam_policy_arn" {
+  description = "The ARN assigned by AWS to this policy."
+  value       = aws_iam_policy.policy.*.arn
+}
+
+output "iam_policy_name" {
+  description = "The name of the policy."
+  value       = aws_iam_policy.policy.*.name
+}
+
+output "iam_policy_path" {
+  description = "The path of the policy in IAM"
+  value       = aws_iam_policy.policy.*.path
+}
+
+output "iam_policy_document" {
+  description = "The policy document."
+  value       = aws_iam_policy.policy.*.policy
+}
---
+++ terraform_module/ps-template/main.tf
@@ -0,0 +1,37 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Locals to create the policies
+# ---------------------------------------------------------------------------------------------------------------------
+
+locals {
+  file = "${var.name}.json"
+  policy_sentry_template = {
+    "mode" : "crud",
+    "read" : var.read_access_level,
+    "write" : var.write_access_level,
+    "list" : var.list_access_level,
+    "tagging" : var.tagging_access_level
+    "permissions-management" : var.permissions_management_access_level,
+    "wildcard-only" : {
+      "single-actions" : var.wildcard_only_single_actions,
+      "service-read" : var.wildcard_only_read_service,
+      "service-write" : var.wildcard_only_write_service,
+      "service-list" : var.wildcard_only_list_service,
+      "service-tagging" : var.wildcard_only_tagging_service,
+      "service-permissions-management" : var.wildcard_only_permissions_management_service,
+    }
+  }
+  rendered_template = jsonencode(local.policy_sentry_template)
+  decoded_template  = jsondecode(jsonencode(local.policy_sentry_template))
+  policy_sentry     = ["policy_sentry", "write-policy", "--fmt", "terraform"]
+  command           = var.minimize ? concat(local.policy_sentry, ["--minimize"]) : local.policy_sentry
+}
+
+resource "local_file" "template" {
+  filename = "template.json"
+  content  = local.rendered_template
+}
+
+data "external" "policy" {
+  program = concat(local.command, ["--input-file", local_file.template.filename])
+}
+
