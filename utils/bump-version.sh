cd4f9d567de76524600b224ff33024fd7146eed1
Kinnaird McQuade <kmcquade@users.noreply.github.com>
2021-04-21 12:56:01 -0400

    Update bump-version.sh

--- utils/bump-version.sh
+++ utils/bump-version.sh
@@ -13,7 +13,7 @@ echo "latest tag: $latest_tag"
 new_tag=$(echo $latest_tag | awk -F. -v a="$1" -v b="$2" -v c="$3" '{printf("%d.%d.%d", $1+a, $2+b , $3+1)}')
 echo "new tag: $new_tag"
 
-echo "__version__ = '$new_tag'" > $version_file
+echo "# pylint: disable=missing-module-docstring\n__version__ = '$new_tag'""" > $version_file
 
 git commit -m "Bump to ${new_tag}"  $version_file || echo "No changes to commit"
 git push origin
