434acec90d6570bad35b3c94f3c38a794dc93dae
gruebel <anton.gruebel@gmail.com>
2024-08-25 18:47:00 +0200

    fix path overrides for DB update

--- .github/scripts/update_data.py
+++ .github/scripts/update_data.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 from policy_sentry.shared import constants
 
-constants.CONFIG_DIRECTORY = "/tmp/.policy_sentry"
-constants.LOCAL_DATASTORE_FILE_PATH = "/tmp/.policy_sentry/iam-definition.json"
-constants.LOCAL_ACCESS_OVERRIDES_FILE = "/tmp/.policy_sentry/access-level-overrides.yml"
-constants.LOCAL_HTML_DIRECTORY_PATH = "/tmp/.policy_sentry/data/docs"
+CONFIG_DIRECTORY = Path("/tmp/.policy_sentry")
+
+constants.CONFIG_DIRECTORY = CONFIG_DIRECTORY
+constants.LOCAL_DATASTORE_FILE_PATH = CONFIG_DIRECTORY / "iam-definition.json"
+constants.LOCAL_ACCESS_OVERRIDES_FILE = CONFIG_DIRECTORY / "access-level-overrides.yml"
+constants.LOCAL_HTML_DIRECTORY_PATH = CONFIG_DIRECTORY / "data/docs"
 
 from policy_sentry.command.initialize import initialize
 
